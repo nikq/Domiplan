0bd6fd4b1967cc39b2212e824ff61516969d5c48
@nikq <nikutama@gmail.com>
2018-07-19 11:38:14 +0900

    implement randoms

--- test/random.cpp
+++ test/random.cpp
@@ -8,6 +8,8 @@
 
 #include <random.hpp>
 
+#include <math.h>
+
 namespace
 {
 const float epsilon = 0.000001f;
@@ -15,18 +17,131 @@ const float epsilon = 0.000001f;
 
 TEST_CASE("xor128", "")
 {
-    SECTION("average=0")
+    SECTION("average=0.5f,count=100")
     {
-        double sum = 0.f;
-
         RANDOM::xor128 r;
 
-        for (int i = 0; i < 1000; i++)
-        {
-            sum += r.rand01() - 0.5f;
-        }
+        double       sum   = 0.f;
+        const int    count = 100;
+        const double eps   = (double)1.f / log2(count);
 
-        REQUIRE_THAT(sum, IsApproxEquals(0.f, epsilon));
+        for (int i = 0; i < count; i++)
+        {
+            sum += r.rand01();
+        }
+        sum = sum / (double)count;
+        printf("sum %f, eps %f\n", sum, eps);
+        REQUIRE(sum == Approx(0.5f).margin(eps));
+    }
+    SECTION("average=0.5f,count=100000")
+    {
+        RANDOM::xor128 r;
+
+        double       sum   = 0.f;
+        const int    count = 100000;
+        const double eps   = (double)1.f / log2(count);
+
+        for (int i = 0; i < count; i++)
+        {
+            sum += r.rand01();
+        }
+        sum = sum / (double)count;
+        printf("sum %f, eps %f\n", sum, eps);
+        REQUIRE(sum == Approx(0.5f).margin(eps));
     }
 }
+
+TEST_CASE("xohiro256**", "")
+{
+    SECTION("average=0.5f,count=100")
+    {
+        RANDOM::xoshiro256aa r;
+
+        double       sum   = 0.f;
+        const int    count = 100;
+        const double eps   = (double)1.f / log2(count);
+
+        for (int i = 0; i < count; i++)
+        {
+            sum += r.rand01();
+        }
+        sum = sum / (double)count;
+        printf("sum %f, eps %f\n", sum, eps);
+        REQUIRE(sum == Approx(0.5f).margin(eps));
+    }
+
+    SECTION("average=0.5f,count=100000")
+    {
+        RANDOM::xoshiro256aa r;
+
+        double       sum   = 0.f;
+        const int    count = 100000;
+        const double eps   = (double)1.f / log2(count);
+
+        for (int i = 0; i < count; i++)
+        {
+            sum += r.rand01();
+        }
+        sum = sum / (double)count;
+
+        printf("sum %f, eps %f\n", sum, eps);
+
+        REQUIRE(sum == Approx(0.5f).margin(eps));
+    }
+
+    SECTION("average=0.5f,count=100000,seed=0")
+    {
+        RANDOM::xoshiro256aa r(0);
+
+        double       sum   = 0.f;
+        const int    count = 100000;
+        const double eps   = (double)1.f / log2(count);
+
+        for (int i = 0; i < count; i++)
+        {
+            sum += r.rand01();
+        }
+        sum = sum / (double)count;
+
+        printf("sum %f, eps %f\n", sum, eps);
+
+        REQUIRE(sum == Approx(0.5f).margin(eps));
+    }
+    SECTION("average=0.5f,count=100000,seed=1")
+    {
+        RANDOM::xoshiro256aa r(1);
+
+        double       sum   = 0.f;
+        const int    count = 100000;
+        const double eps   = (double)1.f / log2(count);
+
+        for (int i = 0; i < count; i++)
+        {
+            sum += r.rand01();
+        }
+        sum = sum / (double)count;
+
+        printf("sum %f, eps %f\n", sum, eps);
+
+        REQUIRE(sum == Approx(0.5f).margin(eps));
+    }
+
+    SECTION("average=0.5f,count=100000,seed=2")
+    {
+        RANDOM::xoshiro256aa r(2);
+
+        double       sum   = 0.f;
+        const int    count = 100000;
+        const double eps   = (double)1.f / log2(count);
+
+        for (int i = 0; i < count; i++)
+        {
+            sum += r.rand01();
+        }
+        sum = sum / (double)count;
+
+        printf("sum %f, eps %f\n", sum, eps);
+
+        REQUIRE(sum == Approx(0.5f).margin(eps));
+    }
 }
